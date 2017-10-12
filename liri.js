7a09edb0adda140ef7297f46b0c794cd4acd687b
Jun Ahn <junha.ahn93@gmail.com>
2017-10-11 23:28:42 -0500

    final push

--- liri.js
+++ liri.js
@@ -107,7 +107,7 @@ function spotify(songName) {
         secret: '82fb5efced714e97b88ab51b401d2685'
       });
        
-      spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+      spotify.search({ type: 'track', query: songName }, function(err, data) {
         if (err) {
           return console.log('Error occurred: ' + err);
         }
@@ -125,23 +125,25 @@ function spotify(songName) {
 
 
 
-function movie(movieName) {
-    var queryUrl = "http://www.omdbapi.com/?t=" + movieName.replace(/ /g, "+") + "&y=&plot=short&apikey=40e9cece";
-	console.log(queryUrl)
-	request(queryUrl, function(error, response, body) {
-		if(!error && response.statusCode === 200){
 
-			console.log("Title: " + JSON.parse(body).Title);
-			console.log("Year: " + JSON.parse(body).Year);
-			console.log("IMDB Rating: " + JSON.parse(body).Ratings[0].Value);
-			console.log("Rotten Tomatos Rating: " + JSON.parse(body).Ratings[1].Value);
-			console.log("Country: " + JSON.parse(body).Country);
-			console.log("Language: " + JSON.parse(body).Language);
-			console.log("Plot: " + JSON.parse(body).Plot);
-			console.log("Actors: " + JSON.parse(body).Actors);
-			console.log("\n")
-		}
-	});
-}
+function movie(movieName) {
+    
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName.replace(/ /g, "+") + "&y=&plot=short&apikey=40e9cece";
+    console.log(queryUrl)
+    request(queryUrl, function(error, response, body) {
+        if(!error && response.statusCode === 200){
+
+            console.log("Title: " + JSON.parse(body).Title);
+            console.log("Year: " + JSON.parse(body).Year);
+            console.log("IMDB Rating: " + JSON.parse(body).Ratings[0]);
+            console.log("Rotten Tomatos Rating: " + JSON.parse(body).Ratings[1]);
+            console.log("Country: " + JSON.parse(body).Country);
+            console.log("Language: " + JSON.parse(body).Language);
+            console.log("Plot: " + JSON.parse(body).Plot);
+            console.log("Actors: " + JSON.parse(body).Actors);
+            console.log("\n")
+        }
+    });
+};
 
 
